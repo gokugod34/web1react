8218a1d80529c469e9a15434c8c21cf93141aa85
MarcosGonzalez <marcosgantonio2007@gmail.com>
2026-07-15 22:34:10 -0300

    US9: Página de producto

--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const apiController = require('../controllers/apiController');
 
 router.get('/products', apiController.listProducts);
+router.get('/products/:id', apiController.getProductById);
+router.put('/products/:id/edit', apiController.updateProduct);
+router.delete('/products/:id/delete', apiController.deleteProduct);
 router.get('/categories', apiController.listCategories);
 
 module.exports = router;
